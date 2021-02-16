410292db2bfccfbdb90321031f7e8481b01d8175
richardjtelford <richard.telford@bio.uib.no>
2021-02-17 00:22:45 +0100

    updated to include continuous predictor

--- StatisticsInR/anova-power.R
+++ StatisticsInR/anova-power.R
@@ -7,11 +7,13 @@ theme_set(theme_bw())
 
 ui <-  fluidPage(
   # Application title
-  titlePanel("Simulating an ANOVA"),
+  titlePanel("Simulating linear regressions"),
   
   sidebarLayout(
+   
     # Sidebar with a slider input
     sidebarPanel(
+      radioButtons("type", label = "Type of predictor variable", choices = c("Categorical", "Continuous"), selected = "Categorical"),
       sliderInput(
         "n",
         "Number of observations:",
@@ -60,42 +62,70 @@ ui <-  fluidPage(
     )
   ))
 
+#### Server ####
 server <- function(input, output) {
+  max_continuous <- 2
   
-  output$samp_size <- renderText(glue::glue("Sample of {input$n} observations for each treatment"))
+  output$samp_size <- renderText({
+    if (input$type == "Categorical") {
+      glue::glue("Sample of {input$n} observations for each treatment")
+    } else {
+      glue::glue("Sample of {input$n} observations") 
+    }
+  })
     
   output$pop_plot <- renderPlot({
-    upper <- max(0, input$delta) + input$sd * 3
-    lower <- min(0, input$delta) - input$sd * 3
     
-    dist <- bind_rows(
-      control = tibble(
-        x = seq(lower, upper, length.out = 101),
-        y = dnorm(x, mean = 0, sd = input$sd)
-      ),
-      treatment = tibble(
-        x = seq(lower, upper, length.out = 101),
-        y = dnorm(x, mean = input$delta, sd = input$sd)
-      ),
-      .id = "treatment"
-    )
-                                        
-    
-    pop_plot <- ggplot(data = dist, aes(x = x, y = y, fill = treatment)) +
-      geom_area(alpha = 0.3, position = "identity") +
-      geom_vline(mapping = aes(xintercept = x, colour = treatment), data = tibble(x = c(0, input$delta), treatment = c("control", "treat")), show.legend = FALSE) +
-      labs(x = "Value", y = "Density", fill = "Treatment") 
-    
+    if (input$type == "Categorical") {
+      upper <- max(0, input$delta) + input$sd * 3
+      lower <- min(0, input$delta) - input$sd * 3
+      
+      dist <- bind_rows(
+        control = tibble(
+          x = seq(lower, upper, length.out = 101),
+          y = dnorm(x, mean = 0, sd = input$sd)
+        ),
+        treatment = tibble(
+          x = seq(lower, upper, length.out = 101),
+          y = dnorm(x, mean = input$delta, sd = input$sd)
+        ),
+        .id = "treatment"
+      )
+      
+      pop_plot <- ggplot(data = dist, aes(x = x, y = y, fill = treatment)) +
+        geom_area(alpha = 0.3, position = "identity") +
+        geom_vline(mapping = aes(xintercept = x, colour = treatment), data = tibble(x = c(0, input$delta), treatment = c("control", "treat")), show.legend = FALSE) +
+        labs(x = "Value", y = "Density", fill = "Treatment") 
+    } else {
+      
+      dist <- tibble(x = seq(0, max_continuous, length.out = 50),
+                     value = x * input$delta) 
+      
+      pop_plot <- ggplot(data = dist, aes(x = x, y = value)) +
+        geom_ribbon(aes(ymin = value - 2 * input$sd, ymax = value + 2 * input$sd), alpha = 0.3) +
+        geom_ribbon(aes(ymin = value - input$sd, ymax = value + input$sd), alpha = 0.3) +
+        geom_line() +
+        labs(x = "Predictor", y = "Response") 
+    }
     pop_plot
   })
   
+  
+  #simulate data
   mods <- reactive({
-    message("reactive called")
-    sims <- rerun(100, tibble(treatment = rep(c("control", "treat"), each = input$n),
-                      value = rnorm(n = input$n * 2, 
-                                    mean = rep(c(0, input$delta), each = input$n), 
-                                    sd = input$sd)
-                      ))
+    if (input$type == "Categorical") {
+      sims <- rerun(100, tibble(treatment = rep(c("control", "treat"), each = input$n),
+                                value = rnorm(n = input$n * 2, 
+                                              mean = rep(c(0, input$delta), each = input$n), 
+                                              sd = input$sd)
+      ))
+    } else { #simulate continuous data
+      sims <- rerun(100, tibble(treatment = runif(input$n, 0, max_continuous),
+                                value = treatment * input$delta + 
+                                  rnorm(n = input$n, mean = 0, sd = input$sd)
+      ))
+    }
+
   
   
   list(
@@ -122,7 +152,7 @@ server <- function(input, output) {
     geom_histogram(bins = 25) +
     geom_vline(xintercept = input$delta) +
     labs(
-      x = "Estimated difference in means",
+      x = if_else(input$type == "Categorical", "Estimated difference in means", "Estimated slope"),
       fill = glue::glue("p < {input$p}"))
 })
  
@@ -132,18 +162,26 @@ server <- function(input, output) {
      geom_point() +
      geom_vline(xintercept = input$delta) +
      geom_vline(xintercept = 0, linetype = "dashed") +
-     labs(x = "Estimated difference in means")
+     labs(x = if_else(input$type == "Categorical", "Estimated difference in means", "Estimated slope"), 
+          y = "Simulation number")
  })
  
  output$sample_plot <- renderPlot({
-   ggplot(mods()$sample, aes(x = treatment, y = value)) + 
-     geom_boxplot(aes(fill = treatment), alpha = 0.3, outlier.shape = NA) + 
-     geom_jitter(aes(colour = treatment), height = 0) +
- #    stat_summary(fun.data = "mean_sd") +
-     # geom_vline(xintercept = input$delta) +
-     # geom_vline(xintercept = 0, linetype = "dashed") +
-     labs(x = "Treatment") +
-     theme(legend.position = "none")
+     if (input$type == "Categorical") {
+       ggplot(mods()$sample, aes(x = treatment, y = value)) + 
+         geom_boxplot(aes(fill = treatment), alpha = 0.3, outlier.shape = NA) + 
+         geom_jitter(aes(colour = treatment), height = 0) +
+         #    stat_summary(fun.data = "mean_sd") +
+         # geom_vline(xintercept = input$delta) +
+         # geom_vline(xintercept = 0, linetype = "dashed") +
+         labs(x = "Treatment", y = "Response") +
+         theme(legend.position = "none")
+     } else {
+       ggplot(mods()$sample, aes(x = treatment, y = value)) + 
+         geom_point() +
+         geom_smooth(method = "lm", formula = y ~ x) +
+         labs(x = "Predictor", y = "Response")
+     }
  })
  
   output
