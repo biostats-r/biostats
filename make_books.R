4dc22a68776254ba1ba687ec5838c3440bb96722
richardjtelford <richard.telford@bio.uib.no>
2021-08-10 20:02:56 +0200

    pipes

--- make_books.R
+++ make_books.R
@@ -3,3 +3,20 @@
 withr::with_dir("DataManagement/", {
   bookdown::render_book('index.Rmd', 'bookdown::bs4_book')
 })
+
+
+# R markdown
+withr::with_dir("Rmarkdown/", {
+  bookdown::render_book('index.Rmd', 'bookdown::bs4_book')
+})
+
+# R packages
+withr::with_dir("writing_a_package/", {
+  bookdown::render_book('index.Rmd', 'bookdown::bs4_book')
+})
+
+#working in R
+
+withr::with_dir("WorkingInR/", {
+  bookdown::render_book('index.Rmd', 'bookdown::bs4_book')
+})
