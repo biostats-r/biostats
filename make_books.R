1f2e0e50df61d28cae180ebb0388bc81d3dff506
richardjtelford <richard.telford@bio.uib.no>
2022-02-25 22:28:53 +0100

    logo and links for front page

--- make_books.R
+++ make_books.R
@@ -1,16 +1,25 @@
-bioceed_links <- function(dir){
-  files <- list.files(dir, pattern = "html", full.names = TRUE, recursive = TRUE)
+bioceed_links <- function(dir, front_page = FALSE){
+  files <- list.files(dir, pattern = "html", full.names = TRUE, recursive = !front_page)
+  print(files)
   map(files, ~{
-    #browser()
     f <- readLines(.x)
-    p1 <- grep(pattern = '</div></main><div class="col-md-3 col-lg-2 d-none d-md-block sidebar sidebar-chapter">', x = f)
-    p2 <- grep(pattern= '<nav id="toc" data-toggle="toc" aria-label="On this page"><h2>On this page</h2>', x = f)
-    print(.x)
+    p1 <- grep(
+      pattern = '</div></main><div class="col-md-3 col-lg-2 d-none d-md-block sidebar sidebar-chapter">',
+      x = f)
+    p2 <- grep(
+      pattern= '<nav id="toc" data-toggle="toc" aria-label="On this page"><h2>On this page</h2>',
+      x = f)
     if (length(p1) == 1 & length(p2) == 1) {
       if (p2 - p1 == 1) {
         f <- c(f[1:p1], 
-               '<p><a href = "https://biostats.w.uib.no/">Biost@ts Homepage</a></p>',
-               '<p><a href = "https://biostats-r.github.io/biostats/">All BioCeed Biost@ts  Books</a></p>', 
+               '<style>
+               img.site-logo {
+  height: auto;
+  width: 95%;
+}
+      </style>',
+               '<p><a  href="https://biostats.w.uib.no/" aria-label="bioST@TS | When biology adds up, at last&#8230;"><img class="site-logo" src="https://biostats.w.uib.no/files/2020/01/biostats-button-res2.png" alt="BioST@TS homepage" width="250" height="101"  data-no-retina class=" attachment-11864" title = "BioST@TS homepage"/></a>   </p>',
+               ifelse(isFALSE(front_page), '<p><a  href="https://biostats.w.uib.no/" aria-label="bioST@TS | When biology adds up, at last&#8230;"><img class="site-logo" src="../figures/bioceed_books_logo.png" alt= "BioST@TS books" width="250" height="101"  data-no-retina class=" attachment-11864", title = "BioST@TS books"/></a>   </p>', ''), 
                f[p2:length(f)])
       }  
     }
@@ -21,10 +30,12 @@ bioceed_links <- function(dir){
 
 # Front page
 bookdown::render_book('index.Rmd', 'bookdown::bs4_book',  output_dir = "docs")
+bioceed_links("docs/", front_page = TRUE)
 
 # Git and GitHub
 withr::with_dir("DataManagement/", {
   bookdown::render_book('index.Rmd', 'bookdown::bs4_book',  output_dir = "../docs/github")
+  
 })
 
 
@@ -32,6 +43,7 @@ withr::with_dir("DataManagement/", {
 withr::with_dir("Rmarkdown/", {
   rmarkdown::render("formatting.Rmd", output_format = "pdf_document")
   bookdown::render_book('index.Rmd', 'bookdown::bs4_book',  output_dir = "../docs/rmarkdown")
+  bioceed_links("../docs/rmarkdown")
 })
 
 # R packages
@@ -45,5 +57,4 @@ withr::with_dir("WorkingInR/", {
   bookdown::render_book('index.Rmd', 'bookdown::bs4_book', output_dir = "../docs/workingInR")
   bioceed_links("../docs/workingInR")
 })
-
 
