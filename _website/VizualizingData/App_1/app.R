26c318f9f092ddd8569f32cd12e707bce33511b2
RichardJTelford <Richard.Telford@uib.no>
2025-01-07 14:58:08 +0100

    more styler

--- _website/VizualizingData/App_1/app.R
+++ _website/VizualizingData/App_1/app.R
@@ -1,9 +1,11 @@
 library("shiny")
 
 ui <- fluidPage(
-  sliderInput(inputId = "num",
-              label = "Choose a number",
-              value = 25, min = 1, max = 100),
+  sliderInput(
+    inputId = "num",
+    label = "Choose a number",
+    value = 25, min = 1, max = 100
+  ),
   plotOutput("hist")
 )
 
