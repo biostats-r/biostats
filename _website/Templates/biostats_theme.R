138c6ffa3c4d7859d48c18eeb7050de99791eba8
RichardJTelford <Richard.Telford@uib.no>
2025-01-07 14:58:08 +0100

    more styler

--- _website/Templates/biostats_theme.R
+++ _website/Templates/biostats_theme.R
@@ -2,5 +2,5 @@
 
 theme_biostats <- theme_set(theme_minimal(base_size = 12))
 
-options(ggplot2.continuous.colour="viridis")
+options(ggplot2.continuous.colour = "viridis")
 options(ggplot2.continuous.fill = "viridis")
